c2fb76e3a6b71cc0b80779ac0feb072778361093
Nguyen Dung <nguyendungzck@gmail.com>
2024-05-06 09:44:59 +0700

    .

--- app/src/main/java/com/percas/studio/example/MyApplication.kt
+++ app/src/main/java/com/percas/studio/example/MyApplication.kt
@@ -8,7 +8,7 @@ import com.percas.studio.template.admob.AppResumeAdsManager
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        AdmobManager.initAdmob(this, timeOut = 10000, isTestAd = true, isEnableAd = true)
+        AdmobManager.initAdmob(this, timeOut = 10000, isEnableAd = true)
         AppResumeAdsManager.getInstance().init(/* application = */ this,/* appOnresmeAdsId = */ "")
     }
 }
