b91b5465c609297880d7e954b623fed40119d915
Nguễn Mạnh Dũng <nguyendungzck@gmail.com>
2024-05-09 12:54:20 +0700

    Update View Control (view background color, background tint color and image tint color

--- Template/src/main/java/com/percas/studio/template/ViewControl.kt
+++ Template/src/main/java/com/percas/studio/template/ViewControl.kt
@@ -1,24 +1,48 @@
 package com.percas.studio.template
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.transition.TransitionManager
 
 object ViewControl {
-    fun View.visible(){
+    fun View.visible() {
         visibility = View.VISIBLE
     }
 
-    fun View.gone(){
+    fun View.gone() {
         visibility = View.GONE
     }
-    fun View.invisible(){
+
+    fun View.invisible() {
         visibility = View.INVISIBLE
     }
 
-    fun ViewGroup.actionAnimation(){
+    fun ViewGroup.actionAnimation() {
         TransitionManager.beginDelayedTransition(this)
+    }
+
+    fun View.setBackgroundColor(colorHex: String) {
+        setBackgroundColor(Color.parseColor(colorHex))
+    }
+
+    fun View.setBackgroundColorFilter(colorHex: String) {
+        backgroundTintList = if (colorHex.isBlank()) {
+            null
+        } else {
+            ColorStateList.valueOf(Color.parseColor(colorHex))
+        }
+    }
+
+    fun ImageView.setColorFilter(colorHex: String) {
+        imageTintList = if (colorHex.isBlank()) {
+            null
+        } else {
+            ColorStateList.valueOf(Color.parseColor(colorHex))
+        }
     }
 
 
